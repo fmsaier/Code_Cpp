10d39dd21a7c598148fa168a4a801d56388a103c
fmsaier <2661458618@qq.com>
2023-11-13 14:03:24 +0800

    .

--- Test_11.4/Test_11.4/test.cpp
+++ Test_11.4/Test_11.4/test.cpp
@@ -15,37 +15,37 @@
 using namespace std;
 
 
-int GetMonDay(int year, int month)
-{
-	int day[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
-	if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
-		day[2] = 29;
-	return day[month];
-}
-int main()
-{
-	int year = 0, month = 0, day = 0;
-	cin >> year >> month >> day;
-	if (year > 9999 || year < 1000 || month>12 || month < 1 || day > GetMonDay(year, month) || day < 1)
-	{
-		return 0;
-	}
-	int i = 1;
-	int _month = 1, _day = 1, _year = year;
-	while (_month != month || _day != day)
-	{
-		_day++;
-		if (_day > GetMonDay(_year, _month))
-		{
-			_month++;
-			_day = 1;
-		}
-		i++;
-	}
-	cout << i;
-	size_t a= 1;
-	return 0;
-}
+//int GetMonDay(int year, int month)
+//{
+//	int day[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+//	if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+//		day[2] = 29;
+//	return day[month];
+//}
+//int main()
+//{
+//	int year = 0, month = 0, day = 0;
+//	cin >> year >> month >> day;
+//	if (year > 9999 || year < 1000 || month>12 || month < 1 || day > GetMonDay(year, month) || day < 1)
+//	{
+//		return 0;
+//	}
+//	int i = 1;
+//	int _month = 1, _day = 1, _year = year;
+//	while (_month != month || _day != day)
+//	{
+//		_day++;
+//		if (_day > GetMonDay(_year, _month))
+//		{
+//			_month++;
+//			_day = 1;
+//		}
+//		i++;
+//	}
+//	cout << i;
+//	size_t a= 1;
+//	return 0;
+//}
 
 
 
@@ -81,3 +81,11 @@ int main()
 //        return sum[n].GetRet();
 //    }
 //};
+
+
+
+int main()
+{
+	printf("%%");
+	return 0;
+}
