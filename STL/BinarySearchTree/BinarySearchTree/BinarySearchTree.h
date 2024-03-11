da686f880857eb0f2aab86adf021f59c8b37f8b7
fmsaier <2661458618@qq.com>
2024-03-11 20:42:08 +0800

    BST写了一半

---
+++ STL/BinarySearchTree/BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+template<class K, class V>
+struct BSTreeNode
+{
+	typedef BSTreeNode<K, V>* PNode;
+
+	PNode _left;
+	PNode _right;
+	K _key;
+	V _value;
+
+	BSTreeNode(K key, V value)
+		:_left(nullptr)
+		,_right(nullptr)
+		,_key(key)
+		,_value(value)
+	{}
+};
+
+template<class K, class V>
+class BSTree
+{
+	typedef BSTreeNode<K, V> Node;
+
+public:
+	bool Insert(const K& key, const V& value)
+	{
+		Node* cur = _root;
+		Node* pre = nullptr;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				pre = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				pre = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		cur = new Node{ key,value };
+		if (pre == nullptr)
+		{
+			_root = cur;
+			return true;
+		}
+		if (cur->_key > pre->_key)
+		{
+			pre->_right = cur;
+		}
+		else
+		{
+			pre->_left = cur;
+		}
+		return true;
+	}
+	Node* Find(const K& key)
+	{
+		if (_root == nullptr)
+			return nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				cur = cur->_left;
+			}
+			else
+			{
+				return cur;
+			}
+		}
+		return nullptr;
+	}
+	bool Erase(const K& key)
+	{
+
+	}
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+		_InOrder(root->_left);
+		cout << root->_key << ":" << root->_value << endl;
+		_InOrder(root->_right);
+	}
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+private:
+	Node* _root = nullptr;
+};
---
+++ STL/BinarySearchTree/BinarySearchTree/test.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include "BinarySearchTree.h"
+
+void TestBSTree()
+{
+	//BSTree<string, string> dict;
+	//dict.Insert("insert", "插入");
+	//dict.Insert("erase", "删除");
+	//dict.Insert("left", "左边");
+	//dict.Insert("string", "字符串");
+	//string str;
+	//while (cin >> str)
+	//{
+	//	auto ret = dict.Find(str);
+	//	if (ret)
+	//	{
+	//		cout << str << ":" << ret->_value << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "单词拼写错误" << endl;
+	//	}
+	//}
+
+	string strs[] = { "苹果", "西瓜", "苹果", "樱桃", "苹果", "樱桃", "苹果", "樱桃", "苹果" };
+	// 统计水果出现的次
+	BSTree<string, int> countTree;
+	for (auto str : strs)
+	{
+		auto ret = countTree.Find(str);
+		if (ret == NULL)
+		{
+			countTree.Insert(str, 1);
+		}
+		else
+		{
+			ret->_value++;
+		}
+	}
+	countTree.InOrder();
+}
+
+int main()
+{
+	TestBSTree();
+	return 0;
+}
