1d368f79c4d6aa2c0a486b0c70437ad8338eb066
fmsaier <2661458618@qq.com>
2024-03-25 16:52:06 +0800

    3.25练习

--- March/Test_3.20/Test_3.20/test.cpp
+++ March/Test_3.20/Test_3.20/test.cpp
@@ -25,100 +25,169 @@ using namespace std;
 
 //可能字符串有错位，那就按照words的每一个string的size去多次滑动窗口就行了
 
+//class Solution {
+//public:
+//    vector<int> findSubstring(string s, vector<string>& words) 
+//    {
+//        unordered_map<string, int> hashp;
+//        for (const auto& str : words)
+//        {
+//            hashp[str]++;
+//        }
+//        int right = 0, left = 0;
+//        int count = 0;
+//        int n = words[0].size();
+//        int k = n;
+//        vector<int> v;
+//        while (k--)
+//        {
+//            right = k;
+//            left = k;
+//            count = 0;
+//            unordered_map<string, int> hashs;
+//            //后面错位了，要用right+n构造string的end，end是左闭右开，所以=是可以的
+//            while (right + n <= s.size())
+//            {
+//                string tmp(s.begin() + right, s.begin() + right + n);
+//                hashs[tmp]++;
+//                if (hashp.count(tmp) && hashs[tmp] <= hashp[tmp])
+//                    count++;
+//                if ((right - left + n) / n > words.size())
+//                {
+//                    tmp = string(s.begin() + left, s.begin() + left + n);
+//                    if (hashp.count(tmp) && hashs[tmp] <= hashp[tmp])
+//                        count--;
+//                    hashs[tmp]--;
+//     /*               if (hashs[tmp] == 0)
+//                        hashs.erase(tmp);*/
+//                    left += n;
+//                }
+//                /*if (hashs.size() == hashp.size())
+//                {
+//                    if (count == words.size())
+//                        v.push_back(left);
+//                }*/
+//                if (count == words.size())
+//                    v.push_back(left);
+//                right += n;
+//            }
+//        }
+//    
+//        return v;
+//    }
+//};
+//
+////class Solution
+////{
+////public:
+////	vector<int> findSubstring(string s, vector<string>& words)
+////	{
+////		vector<int> ret;
+////		unordered_map<string, int> hash1; // 保存 words ⾥⾯所有单词的频次
+////		for (auto& s : words) hash1[s]++;
+////		int len = words[0].size(), m = words.size();
+////		for (int i = 0; i < len; i++) // 执⾏ len 次
+////		{
+////			unordered_map<string, int> hash2; // 维护窗⼝内单词的频次
+////			for (int left = i, right = i, count = 0; right + len <= s.size();
+////				right += len)
+////			{
+////				 进窗⼝ + 维护 count
+////				string in = s.substr(right, len);
+////				hash2[in]++;
+////				if (hash1.count(in) && hash2[in] <= hash1[in]) count++;
+////				 判断
+////				if (right - left + 1 > len * m)
+////				{
+////					 出窗⼝ + 维护 count
+////					string out = s.substr(left, len);
+////					if (hash1.count(out) && hash2[out] <= hash1[out]) count--;
+////					hash2[out]--;
+////					left += len;
+////				}
+////				 更新结果
+////				if (count == m) ret.push_back(left);
+////			}
+////		}
+////		return ret;
+////	}
+////};
+//
+//
+//int main()
+//{
+//    Solution s;
+//    string str = "barfoothefoobarman";
+//    vector<string> words = { "foo","bar" };
+//    s.findSubstring(str, words);
+//    return 0;
+//}
+
 class Solution {
 public:
-    vector<int> findSubstring(string s, vector<string>& words) 
+    string minWindow(string s, string t) 
     {
-        unordered_map<string, int> hashp;
-        for (const auto& str : words)
+        unordered_map<char, int> hash1, hash2;
+        for (auto& ch : t)
         {
-            hashp[str]++;
+            hash2[ch]++;
         }
         int right = 0, left = 0;
         int count = 0;
-        int n = words[0].size();
-        int k = n;
-        vector<int> v;
-        while (k--)
+        int len = INT_MAX;
+        int minRight = -1;
+        bool isOK = false;
+        while (right < s.size())
         {
-            right = k;
-            left = k;
-            count = 0;
-            unordered_map<string, int> hashs;
-            //后面错位了，要用right+n构造string的end，end是左闭右开，所以=是可以的
-            while (right + n <= s.size())
+            hash1[s[right]]++;
+            if (hash2.count(s[right]) && hash1[s[right]] <= hash2[s[right]])
+                count++;
+            while (count == t.size())
             {
-                string tmp(s.begin() + right, s.begin() + right + n);
-                hashs[tmp]++;
-                if (hashp.count(tmp) && hashs[tmp] <= hashp[tmp])
-                    count++;
-                if ((right - left + n) / n > words.size())
+                hash1[s[left]]--;
+
+                if (hash2.count(s[left]) && hash1[s[left]] < hash2[s[left]])
                 {
-                    tmp = string(s.begin() + left, s.begin() + left + n);
-                    if (hashp.count(tmp) && hashs[tmp] <= hashp[tmp])
-                        count--;
-                    hashs[tmp]--;
-     /*               if (hashs[tmp] == 0)
-                        hashs.erase(tmp);*/
-                    left += n;
+                    if (count == t.size())
+                    {
+                        int tmp = len;
+                        len = min(tmp, right - left + 1);
+                        if (len != tmp)
+                            minRight = left;
+                    }
+
+                    count--;
+                    left++;
+                    while (left <= right && !(hash2.count(s[left]) && hash1[s[left]] <= hash2[s[left]]))
+                    {
+                        hash1[s[left]]--;
+                        left++;
+                        isOK = true;
+                    }
                 }
-                /*if (hashs.size() == hashp.size())
-                {
-                    if (count == words.size())
-                        v.push_back(left);
-                }*/
-                if (count == words.size())
-                    v.push_back(left);
-                right += n;
+                if(!isOK)
+                left++;
+                
+
             }
+            isOK = false;
+            right++;
         }
-    
-        return v;
+        if (minRight == -1)
+            return "";
+        else
+        {
+            string tmp(s.begin() + minRight, s.begin() + minRight + len);
+            return tmp;
+        }
     }
 };
-
-//class Solution
-//{
-//public:
-//	vector<int> findSubstring(string s, vector<string>& words)
-//	{
-//		vector<int> ret;
-//		unordered_map<string, int> hash1; // 保存 words ⾥⾯所有单词的频次
-//		for (auto& s : words) hash1[s]++;
-//		int len = words[0].size(), m = words.size();
-//		for (int i = 0; i < len; i++) // 执⾏ len 次
-//		{
-//			unordered_map<string, int> hash2; // 维护窗⼝内单词的频次
-//			for (int left = i, right = i, count = 0; right + len <= s.size();
-//				right += len)
-//			{
-//				 进窗⼝ + 维护 count
-//				string in = s.substr(right, len);
-//				hash2[in]++;
-//				if (hash1.count(in) && hash2[in] <= hash1[in]) count++;
-//				 判断
-//				if (right - left + 1 > len * m)
-//				{
-//					 出窗⼝ + 维护 count
-//					string out = s.substr(left, len);
-//					if (hash1.count(out) && hash2[out] <= hash1[out]) count--;
-//					hash2[out]--;
-//					left += len;
-//				}
-//				 更新结果
-//				if (count == m) ret.push_back(left);
-//			}
-//		}
-//		return ret;
-//	}
-//};
-
 
 int main()
 {
     Solution s;
-    string str = "barfoothefoobarman";
-    vector<string> words = { "foo","bar" };
-    s.findSubstring(str, words);
+    string str = "cabeca";
+    string t = "cae";
+    cout << s.minWindow(str, t);
     return 0;
 }
